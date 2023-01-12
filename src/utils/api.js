6e291033e826dfc189973f8cf84d58e68f70969f
mMay03 <yamkumari.thapa03@gmail.com>
2023-01-12 18:34:55 +0545

    changed key

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,8 +2,10 @@ import axios from "axios";
 
 const BASE_URL = "https://www.googleapis.com/customsearch/v1";
 const params = {
-  key: "AIzaSyCW_XKmLs1PH4k4CpSBUyhcCHF2oZx2DsQ",
-  cx: "e32d195a3884d484e",
+  // key: "AIzaSyCW_XKmLs1PH4k4CpSBUyhcCHF2oZx2DsQ",
+  // cx: "e32d195a3884d484e",
+  key: process.env.REACT_APP_KEY,
+  cx: process.env.REACT_APP_CX,
 };
 export const fetchDataFromApi = async (payload) => {
   const { data } = await axios.get(BASE_URL, {
