7f479e6e163a51afadfda4fcac0f14e87bd30488
mMay03 <yamkumari.thapa03@gmail.com>
2023-01-12 18:48:28 +0545

    remove keys

--- src/utils/api.js
+++ src/utils/api.js
@@ -2,8 +2,6 @@ import axios from "axios";
 
 const BASE_URL = "https://www.googleapis.com/customsearch/v1";
 const params = {
-  // key: "AIzaSyCW_XKmLs1PH4k4CpSBUyhcCHF2oZx2DsQ",
-  // cx: "e32d195a3884d484e",
   key: process.env.REACT_APP_KEY,
   cx: process.env.REACT_APP_CX,
 };
